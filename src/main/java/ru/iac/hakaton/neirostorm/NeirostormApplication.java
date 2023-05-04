decdc5124f23bf24e44bd4a32745f025228b19f2
Evgenii Ponukhov <tweeky92@gmail.com>
2023-05-04 12:43:09 +0300

    First commit

---
+++ src/main/java/ru/iac/hakaton/neirostorm/NeirostormApplication.java
@@ -0,0 +1,13 @@
+package ru.iac.hakaton.neirostorm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeirostormApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeirostormApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/iac/hakaton/neirostorm/NeirostormApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.iac.hakaton.neirostorm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeirostormApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
